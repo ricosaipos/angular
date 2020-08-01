a4ec5f1455636a9885dadfcc66338870371d929e
Ricardo <rnegri@gmail.com>
2020-08-01 13:47:27 -0300

    fix(about): image path and package.json

--- projects/features/about/about/about.component.ts
+++ projects/features/about/about/about.component.ts
@@ -11,9 +11,10 @@ import { ROUTE_ANIMATIONS_ELEMENTS } from '../../../core/core.module';
 export class AboutComponent implements OnInit {
   routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
   // @ts-ignore
-  releaseButler = require('../../../../src/assets/release-butler.png');
+  releaseButler: {default: string} = require('../../../../src/assets/release-butler.png').default
 
-  constructor() {}
+  constructor() {
+  }
 
   ngOnInit() {}
 }
